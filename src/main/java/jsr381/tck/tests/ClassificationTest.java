dbf40317600045f3ddf738d230926475973a0a0b
Kevin Berendsen <kevinberendsen@kramphub.com>
2019-12-03 14:57:12 +0100

    Done with tests for ImplementationService

--- src/main/java/jsr381/tck/tests/ClassificationTest.java
+++ src/main/java/jsr381/tck/tests/ClassificationTest.java
@@ -1,7 +1,13 @@
 package jsr381.tck.tests;
 
+import org.jboss.test.audit.annotations.SpecVersion;
+
 /**
  * @author Kevin Berendsen
  */
+@SpecVersion(spec = "JSR 381", version = "1.0.0")
 public class ClassificationTest {
+
+
+
 }
--- src/main/java/jsr381/tck/tests/spi/ImplementationServiceTest.java
+++ src/main/java/jsr381/tck/tests/spi/ImplementationServiceTest.java
@@ -1,4 +1,42 @@
 package jsr381.tck.tests.spi;
 
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+import javax.visrec.spi.ImplementationService;
+import javax.visrec.spi.ServiceProvider;
+
+import static org.testng.Assert.*;
+
+@SpecVersion(spec = "JSR 381", version = "1.0.0")
 public class ImplementationServiceTest {
+
+    @Test(description = "4.2.1 Must be able to return the name of the implementation which may not be empty nor null.")
+    @SpecAssertion(section = "4.2.1", id = "421-F1")
+    public void testGetName() {
+        try {
+            ServiceProvider serviceProvider = ServiceProvider.current();
+            ImplementationService implementationService = serviceProvider.getImplementationService();
+            assertNotNull(implementationService);
+            assertNotNull(implementationService.getName());
+            assertNotEquals( "", implementationService.getName().trim());
+        } catch (IllegalStateException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test(description = "4.2.1 Must be able to return the version of the implementation which may not be empty nor null.")
+    @SpecAssertion(section = "4.2.1", id = "421-F2")
+    public void testGetVersion() {
+        try {
+            ServiceProvider serviceProvider = ServiceProvider.current();
+            ImplementationService implementationService = serviceProvider.getImplementationService();
+            assertNotNull(implementationService);
+            assertNotNull(implementationService.getVersion());
+            assertNotEquals( "", implementationService.getVersion().trim());
+        } catch (IllegalStateException e) {
+            fail(e.getMessage());
+        }
+    }
 }
