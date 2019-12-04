69c3df1ce8addf748ec4452c5180e99db95e080f
Kevin Berendsen <kevinberendsen@kramphub.com>
2019-12-04 11:52:00 +0100

    Added JSR381Configuration interface to be compatible with each implementation

---
+++ src/main/java/jsr381/tck/spi/JSR381Configuration.java
@@ -0,0 +1,19 @@
+package jsr381.tck.spi;
+
+import javax.visrec.ml.classification.ImageClassifier;
+import java.util.Optional;
+import java.util.ServiceLoader;
+
+public interface JSR381Configuration {
+
+    ImageClassifier.BuildingBlock getABImageClassificationBuildingBlock();
+
+    static JSR381Configuration Load() {
+        ServiceLoader<JSR381Configuration> configurationServiceLoader = ServiceLoader.load(JSR381Configuration.class);
+        Optional<JSR381Configuration> configuration = configurationServiceLoader.findFirst();
+        if (configuration.isEmpty())
+            throw new IllegalStateException("No JSR381Configuration implementations are found");
+        return configuration.get();
+    }
+
+}
